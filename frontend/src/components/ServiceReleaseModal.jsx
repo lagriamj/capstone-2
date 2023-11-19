28a7d6621f76bb506b3a67b5a663f172e26fd12d
Mark John Lagria <90015916+lagriamj@users.noreply.github.com>
2023-11-19 12:35:43 +0800

    Update ServiceReleaseModal.jsx

--- frontend/src/components/ServiceReleaseModal.jsx
+++ frontend/src/components/ServiceReleaseModal.jsx
@@ -145,7 +145,6 @@ const ServiceReleaseModal = ({
       title={
         <div className="flex justify-between items-center">
           <span>CITC TECHNICAL SERVICE REQUEST SLIP</span>
-          <span>REQUEST ID: {data.request_code}</span>
         </div>
       }
       centered
@@ -155,6 +154,12 @@ const ServiceReleaseModal = ({
       <div className="relative p-6 text-lg">
         {data && (
           <Row gutter={[16, 16]}>
+            <Col xs={24} lg={6}>
+              <label className="block text-sm font-bold mb-2 text-black">
+                Request ID
+              </label>
+              <Input value={data.request_code} readOnly className="h-[40px]" />
+            </Col>
             <Col xs={24} lg={6}>
               <label className="block text-sm font-bold mb-2 text-black">
                 Requesting Office
