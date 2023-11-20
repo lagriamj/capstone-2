2dac25233546551a3cc5d2b872a36138461023c7
Mark John Lagria <90015916+lagriamj@users.noreply.github.com>
2023-11-20 22:05:41 +0800

    Update PrintDelayedReport.jsx

--- frontend/src/components/PrintDelayedReport.jsx
+++ frontend/src/components/PrintDelayedReport.jsx
@@ -6,11 +6,12 @@ import ReactToPrint from "react-to-print";
 const PrintDelayedReport = ({
   isOpen,
   onClose,
-  tableColumn,
   techData,
   pageSize,
   currentPage,
   isLargeScreen,
+  startDate,
+  endDate,
 }) => {
   const contentRef = useRef();
   const printRef = useRef();
@@ -21,12 +22,62 @@ const PrintDelayedReport = ({
     }
   };
 
+  const columns = [
+    {
+      title: "#",
+      dataIndex: "rowIndex",
+      key: "rowIndex",
+      align: "center",
+      render: (text, record, index) => index + 1,
+    },
+    {
+      title: "Request ID",
+      dataIndex: "request_code",
+      key: "request_code",
+    },
+    {
+      title: "Date Requested",
+      dataIndex: "dateRequested",
+      key: "dateRequested",
+    },
+    {
+      title: "Nature of Request",
+      dataIndex: "natureOfRequest",
+      key: "natureOfRequest",
+    },
+    {
+      title: "Serviced By",
+      dataIndex: "serviceBy",
+      key: "serviceBy",
+    },
+    {
+      title: "ARTA",
+      dataIndex: "arta",
+      key: "arta",
+    },
+  ];
+
+  const fromDateObj = startDate ? new Date(startDate) : null;
+  const toDateObj = endDate ? new Date(endDate) : null;
+
+  // Helper function to format a Date object as "Month day, year"
+  const formatDate = (dateObj) => {
+    const options = { year: "numeric", month: "long", day: "numeric" };
+    return dateObj?.toLocaleDateString(undefined, options);
+  };
+
+  // Format the date range for display
+  const formattedDateRange =
+    fromDateObj && toDateObj
+      ? ` ${formatDate(fromDateObj)} - ${formatDate(toDateObj)}`
+      : "";
+
   return (
     <Modal
       title={
         <div className="flex items-center gap-2 font-sans">
           {" "}
-          <span>Delayed Report Print Preview</span>{" "}
+          <span>Delayed Request Report Print Preview</span>{" "}
           <button
             className="text-white px-5 py-2 ml-auto mr-5 rounded-md border-1 border-gray-300 bg-main hover:opacity-90"
             onClick={handlePrint}
@@ -61,10 +112,14 @@ const PrintDelayedReport = ({
           <h3 className="text-base font-bold">
             Computer Equipment Maintenance and Systems Engineering Division
           </h3>
-          <h1 className="text-base font-bold">Delayed Report</h1>
+          <h1 className="text-base font-bold">Delayed Request Report</h1>
+          <div className="flex items-center justify-center gap-1">
+            <h3 className="text-base">For the period</h3>
+            <h1 className="font-bold text-base">{formattedDateRange}</h1>
+          </div>
         </div>
         <Table
-          columns={tableColumn}
+          columns={columns}
           dataSource={techData.map((item, index) => ({
             ...item,
             key: index,
@@ -84,11 +139,12 @@ const PrintDelayedReport = ({
 PrintDelayedReport.propTypes = {
   isOpen: PropTypes.bool.isRequired,
   onClose: PropTypes.func.isRequired,
-  tableColumn: PropTypes.any.isRequired,
   techData: PropTypes.any.isRequired,
   pageSize: PropTypes.any.isRequired,
   currentPage: PropTypes.any.isRequired,
   isLargeScreen: PropTypes.bool.isRequired,
+  startDate: PropTypes.any,
+  endDate: PropTypes.any,
 };
 
 export default PrintDelayedReport;
