0a9dd54141fc87c670e5244ce9ff771d4965e7f9
Mark John Lagria <90015916+lagriamj@users.noreply.github.com>
2023-12-05 17:26:30 +0800

    Update HeadTransactions.jsx

--- frontend/src/views/head/HeadTransactions.jsx
+++ frontend/src/views/head/HeadTransactions.jsx
@@ -29,6 +29,7 @@ const HeadTransactions = () => {
 
   const [viewRating, setViewRating] = useState(false);
   const [viewRatingModal, setViewRatingModal] = useState(false);
+  const [passRequestCode, setPassRequestCode] = useState(false);
 
   const [cancel, setCancel] = useState(false);
   const [viewCancel, setViewCancel] = useState(false);
@@ -53,8 +54,9 @@ const HeadTransactions = () => {
     setPurgedReq(true);
   };
 
-  const handleViewRating = (id) => {
+  const handleViewRating = (id, request_code) => {
     setViewRatingModal(id);
+    setPassRequestCode(request_code);
     setViewRating(true);
   };
 
@@ -399,7 +401,7 @@ const HeadTransactions = () => {
           ) : record.status === "Closed" ? (
             doneRating.some((rating) => rating.request_id === record.id) ? (
               <button
-                onClick={() => handleViewRating(record.id)}
+                onClick={() => handleViewRating(record.id, record.request_code)}
                 className="text-white text-base bg-gray-400 py-2 px-3 rounded-lg"
               >
                 <FontAwesomeIcon icon={faStar} />
